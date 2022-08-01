5e66948d5d8cc5c5caacb1628b0fa6d624729d76
PaulGLujan <37720729+PaulGLujan@users.noreply.github.com>
2022-08-01 14:02:11 -0700

    Cleaning up code names

--- 01-challenge-problem.py
+++ 01-challenge-problem.py
@@ -1,35 +1,35 @@
 class Point:
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX
-        self.coordY = coordY
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
 
 
 class Rectangle:
-    def __init__(self, starting_point, broad, high):
+    def __init__(self, starting_point, width, height):
         self.starting_point = starting_point
-        self.broad = broad
-        self.high = high
+        self.width = width
+        self.height = height
 
     def area(self):
-        return self.broad * self.high
+        return self.width * self.height
 
     def end_points(self):
-        top_right = self.starting_point.coordX + self.broad
-        bottom_left = self.starting_point.coordY + self.high
-        print('Starting Point (X)): ' + str(self.starting_point.coordX))
-        print('Starting Point (Y)): ' + str(self.starting_point.coordY))
+        top_right = self.starting_point.x_coordinate + self.width
+        bottom_left = self.starting_point.y_coordinate + self.height
+        print('Starting Point (X)): ' + str(self.starting_point.x_coordinate))
+        print('Starting Point (Y)): ' + str(self.starting_point.y_coordinate))
         print('End Point X-Axis (Top Right): ' + str(top_right))
         print('End Point Y-Axis (Bottom Left): ' + str(bottom_left))
 
 
-def build_stuff():
+def build_rectangle():
     main_point = Point(50, 100)
     rect = Rectangle(main_point, 90, 10)
 
     return rect
 
 
-my_rect = build_stuff()
+my_rect = build_rectangle()
 
 print(my_rect.area())
 my_rect.end_points()
