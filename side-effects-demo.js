436974147a8827fce64ada3ff01c044e0c1c8a0d
PaulGLujan <37720729+PaulGLujan@users.noreply.github.com>
2022-08-08 21:01:50 -0700

    side-effects demo

--- side-effects-demo.js
+++ side-effects-demo.js
@@ -1,23 +1,23 @@
-function connectDatabase() {
-  const didConnect = database.connect();
+function connectDatabase() { // SO
+  const didConnect = database.connect(); // SO
   if (didConnect) {
     return true;
   } else {
-    console.log('Could not connect to database!');
+    console.log('Could not connect to database!'); // SO
     return false;
   }
 }
 
-function determineSupportAgent(ticket) {
+function determineSupportAgent(ticket) { // NSO
   if (ticket.requestType === 'unknown') {
-    return findStandardAgent();
+    return findStandardAgent(); // NSO
   }
-  return findAgentByRequestType(ticket.requestType);
+  return findAgentByRequestType(ticket.requestType); // NSO
 }
 
-function isValid(email, password) {
+function isValid(email, password) { // NSO
   if (!email.includes('@') || password.length < 7) {
-    console.log('Invalid input!');
+    console.log('Invalid input!'); // SO
     return false;
   }
   return true;
